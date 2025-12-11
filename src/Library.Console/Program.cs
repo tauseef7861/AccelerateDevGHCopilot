ba9e1085d46077904a37ce8438573ae232b57ff1
Tauseef Ahamed <ahamedtauseef786@gmail.com>
2025-12-11 02:27:52 -0800

    Add book search functionality and update dependency injection in ConsoleApp

--- src/Library.Console/Program.cs
+++ src/Library.Console/Program.cs
@@ -1,26 +1,34 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Library.ApplicationCore;
 using Library.Infrastructure.Data;
-using Library.ApplicationCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 var services = new ServiceCollection();
 
-var configuration = new ConfigurationBuilder()
-.SetBasePath(Directory.GetCurrentDirectory())
-.AddJsonFile("appSettings.json")
-.Build();
-
-services.AddSingleton<IConfiguration>(configuration);
-
-services.AddScoped<IPatronRepository, JsonPatronRepository>();
-services.AddScoped<ILoanRepository, JsonLoanRepository>();
-services.AddScoped<ILoanService, LoanService>();
-services.AddScoped<IPatronService, PatronService>();
-
+// Register JsonData as singleton
 services.AddSingleton<JsonData>();
+
+// Register repositories
+services.AddSingleton<IPatronRepository, JsonPatronRepository>();
+services.AddSingleton<ILoanRepository, JsonLoanRepository>();
+
+// Register services
+services.AddSingleton<ILoanService, LoanService>();
+services.AddSingleton<IPatronService, PatronService>();
+
+// Register configuration
+services.AddSingleton<IConfiguration>(config);
+
+// Register ConsoleApp
 services.AddSingleton<ConsoleApp>();
 
-var servicesProvider = services.BuildServiceProvider();
+// Build service provider
+var serviceProvider = services.BuildServiceProvider();
 
-var consoleApp = servicesProvider.GetRequiredService<ConsoleApp>();
-consoleApp.Run().Wait();
+// Get and run the ConsoleApp
+var consoleApp = serviceProvider.GetRequiredService<ConsoleApp>();
+await consoleApp.Run();
